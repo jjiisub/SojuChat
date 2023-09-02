34c2cd396a098e4f40c2e1edd1d1374688bafbbd
LeeJiSeop <jjiisub@naver.com>
2023-09-02 19:06:31 +0900

    modal test

--- api/views.py
+++ api/views.py
@@ -28,42 +28,15 @@ class InsertVoteElemnt(APIView):
             keyFreq.save()
 
         res = {
-            "text": "Would you like to play a game?",
+            "text": f"{user_nick} 님의 관심 키워드는 {keywords} 입니다",
             "attachments": [
                 {
-                    "text": "Choose a game to play",
-                    "fallback": "You are unable to choose a game",
-                    "callback_id": "wopr_game",
-                    "color": "#3AA3E3",
-                    "attachment_type": "default",
-                    "actions": [
-                        {
-                            "name": "game",
-                            "text": "Chess",
-                            "type": "button",
-                            "value": "chess"
-                        },
-                        {
-                            "name": "game",
-                            "text": "Falken's Maze",
-                            "type": "button",
-                            "value": "maze"
-                        },
-                        {
-                            "name": "game",
-                            "text": "Thermonuclear War",
-                            "style": "danger",
-                            "type": "button",
-                            "value": "war",
-                            "confirm": {
-                                "title": "Are you sure?",
-                                "text": "Wouldn't you prefer a good game of chess?",
-                                "ok_text": "Yes",
-                                "dismiss_text": "No"
-                            }
-                        }
-                    ]
-                }
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "<https://naver.com|결과보기>"
+			        }
+		        }
             ]
         }
         
