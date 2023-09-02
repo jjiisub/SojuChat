888d7e1b47fea6e51bfd1f6a1ed4da6792abe248
LeeJiSeop <jjiisub@naver.com>
2023-09-02 18:58:54 +0900

    modal test

--- api/views.py
+++ api/views.py
@@ -26,6 +26,47 @@ class InsertVoteElemnt(APIView):
             keyFreq, _ = KeywordFreq.objects.get_or_create(keyword=keyword.strip())
             keyFreq.freq = keyFreq.freq + 1
             keyFreq.save()
+
+        res = {
+            "text": "Would you like to play a game?",
+            "attachments": [
+                {
+                    "text": "Choose a game to play",
+                    "fallback": "You are unable to choose a game",
+                    "callback_id": "wopr_game",
+                    "color": "#3AA3E3",
+                    "attachment_type": "default",
+                    "actions": [
+                        {
+                            "name": "game",
+                            "text": "Chess",
+                            "type": "button",
+                            "value": "chess"
+                        },
+                        {
+                            "name": "game",
+                            "text": "Falken's Maze",
+                            "type": "button",
+                            "value": "maze"
+                        },
+                        {
+                            "name": "game",
+                            "text": "Thermonuclear War",
+                            "style": "danger",
+                            "type": "button",
+                            "value": "war",
+                            "confirm": {
+                                "title": "Are you sure?",
+                                "text": "Wouldn't you prefer a good game of chess?",
+                                "ok_text": "Yes",
+                                "dismiss_text": "No"
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
         
 
-        return Response(f"{user_nick} 님의 관심 키워드는 {keywords} 입니다")
+        return Response(res)
+        # return Response(f"{user_nick} 님의 관심 키워드는 {keywords} 입니다")
